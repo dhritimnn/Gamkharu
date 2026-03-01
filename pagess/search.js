22a3e0441b5e70ed537ddc4d52991de9fc88b253
dhritimnn <dhritimankashyap20@gmail.com>
2026-03-01 12:56:56 +0530

    update

--- pagess/search.js
+++ pagess/search.js
@@ -9,13 +9,27 @@ async function products() {
         ../database/${e.url}" alt=""><span class="about"><h6>${e.name}</h6><span class="tags"><p>${e.tags[0]}</p><p>${e.tags[1]}</p></span><h5>${e.price}</h5></span></a>`;
     }
   )
-  box.innerHTML += `
-            <a href="" style="padding: 0 4rem;"><h4>
-          Explore More
-            </h4> </a>
-      `
 }
 products();
 
 
+let searchspace = document.querySelector('#searchspace');
 
+searchspace.addEventListener('keyup', () => {
+    let result = [];
+    let input = searchspace.value;
+    if (input.length) {
+      result = availableKeywords.filter((data) => {
+        return keyword.toLowerCase().includes(input.toLowerCase());
+      });
+      console.log(result);
+    }
+})
+
+
+
+
+function display(result) {
+  const content = result.map((list) => {});
+  return "<li>" + list + "</li>";
+}
