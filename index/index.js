b0e049e28db7f8b6baa5a5d4458784d526231eab
dhritimnn <dhritimankashyap20@gmail.com>
2026-03-01 07:29:47 +0530

    update

--- index/index.js
+++ index/index.js
@@ -6,13 +6,13 @@ window.addEventListener('load', function() {
 
 async function featured() {
   const box = document.querySelector('#ftrdbtns');
-  const response = await fetch("database/featured.json");
+  const response = await fetch("../database/featured.json");
   const data = await response.json(); // Now it's a JS object
   data.forEach(
     (e) => {
       box.innerHTML += `
       <a href=""><img src="
-        database/${e.url}" alt=""><span class="about"><h6>${e.name}</h6><span class="tags"><p>${e.tags[0]}</p><p>${e.tags[1]}</p></span><h5>${e.price}</h5></span></a>`;
+        ../database/${e.url}" alt=""><span class="about"><h6>${e.name}</h6><span class="tags"><p>${e.tags[0]}</p><p>${e.tags[1]}</p></span><h5>${e.price}</h5></span></a>`;
     }
   )
   box.innerHTML += `
@@ -31,13 +31,13 @@ featured();
 
 async function carousel() {
   const box = document.querySelector('#crcl');
-  const response = await fetch("database/carousel.json");
+  const response = await fetch("../database/carousel.json");
   const data = await response.json(); // Now it's a JS object
   data.forEach(
     (e) => {
       box.innerHTML += `
       <div class="carousel-item active">
-          <img src="database/${e}" class="d-block w-100" alt="...">
+          <img src="../database/${e}" class="d-block w-100" alt="...">
         </div>
       `
     }
