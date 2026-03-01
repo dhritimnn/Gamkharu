12f93dcbfe4d982fbe02eeb896854959d322f4dc
dhritimnn <dhritimankashyap20@gmail.com>
2026-03-01 07:24:37 +0530

    update

--- index/index.js
+++ index/index.js
@@ -6,13 +6,13 @@ window.addEventListener('load', function() {
 
 async function featured() {
   const box = document.querySelector('#ftrdbtns');
-  const response = await fetch("https://dhritimnn.github.io/Gamkharu-database/featured.json");
+  const response = await fetch("database/featured.json");
   const data = await response.json(); // Now it's a JS object
   data.forEach(
     (e) => {
       box.innerHTML += `
       <a href=""><img src="
-        https://dhritimnn.github.io/Gamkharu-database/${e.url}" alt=""><span class="about"><h6>${e.name}</h6><span class="tags"><p>${e.tags[0]}</p><p>${e.tags[1]}</p></span><h5>${e.price}</h5></span></a>`;
+        database/${e.url}" alt=""><span class="about"><h6>${e.name}</h6><span class="tags"><p>${e.tags[0]}</p><p>${e.tags[1]}</p></span><h5>${e.price}</h5></span></a>`;
     }
   )
   box.innerHTML += `
@@ -29,16 +29,15 @@ featured();
 
 
 
-
 async function carousel() {
   const box = document.querySelector('#crcl');
-  const response = await fetch("https://dhritimnn.github.io/Gamkharu-database/carousel.json");
+  const response = await fetch("database/carousel.json");
   const data = await response.json(); // Now it's a JS object
   data.forEach(
     (e) => {
       box.innerHTML += `
       <div class="carousel-item active">
-          <img src="https://dhritimnn.github.io/Gamkharu-database/${e}" class="d-block w-100" alt="...">
+          <img src="database/${e}" class="d-block w-100" alt="...">
         </div>
       `
     }
