1f12ea9b245ffc207a1ed5900e37c041f24fd3da
dhritimnn <dhritimankashyap20@gmail.com>
2026-04-18 11:30:08 +0530

    update subcatagory.js

--- catagories/subcatagory.js
+++ catagories/subcatagory.js
@@ -1,9 +1,6 @@
 async function load() {
   
   await addcomp('navbar-placeholder', '../comps/nav.html');
-  await addcomp('searchbar-placeholder', '../comps/searchbar.html');
-  
-  await searchjsfunc();
   
   await addcomp('footer-placeholder', '../comps/footer.html');
 
